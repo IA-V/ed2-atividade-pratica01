1f4358eb77d3e695e69ea21d97554f584a5518de
IA-V <iagoviper48@gmail.com>
2022-05-03 16:55:57 -0300

    Resolução da 2º questão

--- src/main/Main.java
+++ src/main/Main.java
@@ -4,19 +4,24 @@ import ordenacao.*;
 
 public class Main {
 	public static void main (String [] args) {
-		Estrutura<Integer, String>[] vetor = new Estrutura[5];
-		vetor[0] = new Estrutura<>(3, "cafe");
-		vetor[1] = new Estrutura<>(1, "agua");
-		vetor[2] = new Estrutura<>(3, "colchao");
-		vetor[3] = new Estrutura<>(5, "espada");
-		vetor[4] = new Estrutura<>(4, "duelo");
+		Estrutura<Integer, String>[] vetor = new Estrutura[10];
+		vetor[0] = new Estrutura<>(5, "cafe");
+		vetor[1] = new Estrutura<>(2, "agua");
+		vetor[2] = new Estrutura<>(4, "colchao");
+		vetor[3] = new Estrutura<>(3, "espada");
+		vetor[4] = new Estrutura<>(9, "duelo");
+		vetor[5] = new Estrutura<>(6, "duelo");
+		vetor[6] = new Estrutura<>(10, "duelo");
+		vetor[7] = new Estrutura<>(8, "duelo");
+		vetor[8] = new Estrutura<>(1, "duelo");
+		vetor[9] = new Estrutura<>(7, "duelo");
 		
 		for(int i = 0; i < vetor.length; i++) {
 			System.out.println("Valor = " + vetor[i].getValor() + "\nChave = " + vetor[i].getChave() + "\n");
 		}
 		System.out.println("---------------------------------\n---------------------------------\n");
 		
-		SelectionSort.ordenar(vetor); // Talvez, para valores muito pequenos, o selectionsort seja estavel
+		SelectionSort.ordenarMinMax(vetor); // Talvez, para valores muito pequenos, o selectionsort seja estavel
 		//InsertionSort.ordenar(vetor);
 		//MergeSort.ordenar(vetor);
 		//QuickSort.ordenar(vetor);
--- src/ordenacao/SelectionSort.java
+++ src/ordenacao/SelectionSort.java
@@ -1,5 +1,6 @@
 package ordenacao;
 
+import java.lang.Math;
 import armazenamento.Estrutura;
 
 public class SelectionSort {
@@ -8,7 +9,7 @@ public class SelectionSort {
 			int min = i;
 			for(int j = i+1; j < vetor.length; j++) {
 				if(vetor[j].compararCom(vetor[min].getChave()) < 0) {
-					// Se o retorno do método for menor que 1, então o valor que está em "j" é menor que o que está em "min"
+					// Se o retorno do método for menor que 0, então o valor que está em "j" é menor que o que está em "min"
 					min = j;
 				}
 			}
@@ -16,6 +17,52 @@ public class SelectionSort {
 			Estrutura<?, ?> aux = vetor[i];
 			vetor[i] = vetor[min];
 			vetor[min] = aux;
+			
+			for(int m = 0; m < vetor.length; m++) {
+				System.out.println("[" + vetor[m].getChave() + "]");
+			}
+			System.out.println();
+		}
+	}
+	
+	public static void ordenarMinMax(Estrutura<?, ?>[] vetor) {		
+		/* Inicializa "i" com o primeiro índice e "k" com o último índice.
+		 * A condição é "i < k" porque, a partir de "k" até o último elemento, o array já estará ordenado.
+		 */
+		for(int i = 0, k = vetor.length-1; i < k; i++, k--) {
+			int min = i;
+			int max = k;
+			
+			/* Aqui no "for" interno, "j < k" se deve ao fato de que não é necessário sempre iterar sobre todo o array, pois de "k" até "vetor.length-1",
+			 * os elementos serão sempre maiores que "min". O mesmo se aplica de "k-1" até o 1º elemento.
+			 */
+			for(int j = i+1, l = k-1; j < k; j++, l--) {
+				
+				// Se o retorno do método for menor que 0, então o valor que está em "j" é menor que o que está em "min"
+				if(vetor[j].compararCom(vetor[min].getChave()) < 0) {
+					min = j;
+				}
+				
+				// Se o retorno do método for maior que 0, então o valor que está em "l" é maior que o que está em "max"
+				if(vetor[l].compararCom(vetor[max].getChave()) > 0) {
+					max = l;
+				}
+			}
+			
+			Estrutura<?, ?> aux = vetor[i];
+			vetor[i] = vetor[min];
+			vetor[min] = aux;
+			
+			/* Se "max" for igual a "i", "max" deve receber "min" porque "i" e "min" já tiveram seus valores trocados nas linhas 46-48, o que significa que
+			 * o maior valor está, agora, em "min".
+			 */
+			if(max == i) {
+				max = min;
+			}
+			
+			aux = vetor[k];
+			vetor[k] = vetor[max];
+			vetor[max] = aux;
 		}
 	}
 }
